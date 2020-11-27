d41de00bf1225e495d72636650a7dce9ec2973d4
unknown <shubhra498@gmail.com>
2020-11-28 00:37:50 +0530

    edit pricing section

--- resources/views/pages/checkin.blade.php
+++ resources/views/pages/checkin.blade.php
@@ -24,25 +24,26 @@
     </div>
 </div>
 <div class="container" style="margin-top:50px">
-    <div class="row">
-        <div class="col-sm-9">
-            <h1>{{$hotel->name}}</h1>
-            <p style="color: #565656;margin-bottom:1px;margin-left:5px"><i class="fas fa-hotel"></i>&nbsp;{{$hotel->category}}&nbsp;&nbsp;&nbsp;<i class="fas fa-map-marker-alt"></i>&nbsp;{{$hotel->city}}</p>
-            <div class="card" style="margin-top:50px">
-                <div class="card-body">
-                    <h5 class="card-title">Description</h5>
-                    <p>{{$hotel->description}}</p>
-                    <div style="margin-bottom:10px">
-                        @if ($hotel->ac==="1") <span class="badge badge-dark"><i class="fas fa-wind"></i>&nbsp;AC &nbsp;</span> @endif
-                        @if ($hotel->tv==="1") <span class="badge badge-dark"><i class="fas fa-tv"></i>&nbsp;TV &nbsp;</span> @endif
-                        @if ($hotel->wifi==="1") <span class="badge badge-dark"><i class="fas fa-wifi"></i>&nbsp;WIFI &nbsp;</span> @endif
+    <form method="POST">
+        <div class="row">
+            <div class="col-sm-9">
+                <h1>{{$hotel->name}}</h1>
+                <p style="color: #565656;margin-bottom:1px;margin-left:5px"><i class="fas fa-hotel"></i>&nbsp;{{$hotel->category}}&nbsp;&nbsp;&nbsp;<i class="fas fa-map-marker-alt"></i>&nbsp;{{$hotel->city}}</p>
+                <div class="card" style="margin-top:50px">
+                    <div class="card-body">
+                        <h5 class="card-title">Description</h5>
+                        <p>{{$hotel->description}}</p>
+                        <div style="margin-bottom:10px">
+                            @if ($hotel->ac==="1") <span class="badge badge-dark"><i class="fas fa-wind"></i>&nbsp;AC &nbsp;</span> @endif
+                            @if ($hotel->tv==="1") <span class="badge badge-dark"><i class="fas fa-tv"></i>&nbsp;TV &nbsp;</span> @endif
+                            @if ($hotel->wifi==="1") <span class="badge badge-dark"><i class="fas fa-wifi"></i>&nbsp;WIFI &nbsp;</span> @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card" style="margin-top:50px">
-                <div class="card-body">
-                    <h5 class="card-title">Booking Details</h5>
-                    <form method="POST">
+                <div class="card" style="margin-top:50px">
+                    <div class="card-body">
+                        <h5 class="card-title">Booking Details</h5>
+                        
                         @csrf
                         <input type="hidden" name="hotel_id" value="{{$hotel->id}}" />
                         <div class="form-group">
@@ -71,20 +72,29 @@
                                 <input type="time" class="form-control" name='checkout_time' id="checkout_time">
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total</h5>
+                        <p id="price_text">&#x20B9; {{$hotel->price}}</p>
+                        <button class="btn btn-success">Book Now</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Total</h5>
-                    <button class="btn btn-success">Book Now</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    </form>
 </div>
+@endsection
+@section('script')
+<script type="text/javascript">
+    const price = {{$hotel->price}};
+    $(document).ready(function() {
+       $('#no_room').change(function(e) {
+            $('#price_text').html(`&#x20B9; ${$('#no_room').val() * price}`)
+       });
+    })
+</script>
 @endsection
